9221e483082d54255ca02cc84b60935d22562d44
deadmanbg <35937095+deadmanbg@users.noreply.github.com>
2021-01-09 18:58:12 +0200

    Add files via upload

---
+++ connection.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+import urllib.request
+import time
+import socket
+
+on = True
+start_time = start_time_str = None
+while True:
+    try:
+        time.sleep(1)
+        s = socket.socket()
+        s.settimeout(1)
+        s.connect(('8.8.8.8', 53))
+        if on == False:
+            print('Online')
+            end_time = datetime.now()
+            end_time_str = end_time.strftime("%Y %m %d %H:%M:%S")
+            with open('offline.txt', 'a') as f:
+                f.write(f'{start_time_str} - {end_time_str}: total {end_time - start_time}\n')
+        s.close()
+        on = True
+    except Exception:
+        if on:
+            on = False
+            print('Offline')
+            start_time = datetime.now()
+            start_time_str = start_time.strftime("%d.%m.%Y %H:%M:%S")
