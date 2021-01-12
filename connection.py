6bbbb03707c1e0ecdd0198212c2d83f4d56fbadf
deadmanbg <35937095+deadmanbg@users.noreply.github.com>
2021-01-12 22:52:11 +0200

    Add files via upload

--- connection.py
+++ connection.py
@@ -12,18 +12,18 @@ while True:
         s.settimeout(1)
         s.connect(('8.8.8.8', 53))
         if on == False:
-            print('Online')
             end_time = datetime.now()
             end_time_str = end_time.strftime("%Y %m %d %H:%M:%S")
+            print('Online ' + end_time_str)
             with open('offline.txt', 'a') as f:
                 total = end_time - start_time
-                if (total.total_seconds() > 2)
-                    f.write(f'{start_time_str} - {end_time_str}: total {total}\n')
+                if total.total_seconds() > 2:
+                    f.write(start_time_str + ' - ' + end_time_str + ': total ' + str(total) + '\n')
         s.close()
         on = True
     except Exception:
         if on:
             on = False
-            print('Offline')
             start_time = datetime.now()
             start_time_str = start_time.strftime("%d.%m.%Y %H:%M:%S")
+            print('Offline ' + start_time_str)
